23819a50c76ccc7066209c4324d027d0c4a5685a
vvvt <shiftvt@gmail.com>
2017-08-03 17:29:26 +0200

    ADD Test for One Piece Chapter Detection

--- js/block.js
+++ js/block.js
@@ -22,8 +22,17 @@ $(document).ready(function () {
         });
     });
 
+    /*
     $.getJSON("https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1", function (result) {
         $('#greeting').html(result.data.children[0].data.title + ' <a href="' + result.data.children[0].data.url + '">&#119101;</a>');
     });
-
+    */
+    $.getJSON("https://www.reddit.com/r/OnePiece/hot.json?limit=1", function (result) {
+        var firstEntry = result.data.children[0].data.title.substring(19,20);
+        var secondEntry = result.data.children[1].data.title.substring(19,20);
+        
+        if(firstEntry !== "8" && secondEntry!== "8"){
+            $('#greeting').html("New Chapter is still offline")
+        }
+    });
 });
