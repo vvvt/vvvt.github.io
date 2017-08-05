a7bf136b11f247e4881eb2fba813ac76597c5e36
vvvt <shiftvt@gmail.com>
2017-08-06 01:58:31 +0200

    DELETE broken todo list

--- js/block.js
+++ js/block.js
@@ -18,14 +18,6 @@ $(document).ready(function () {
         }
     });
 
-    $('#newTodoButton').click(function () {
-        var toAdd = $('input[name=newTodoText]').val();
-        $('#todoList').prepend('<div class="todoItem"><p>' + toAdd + '</p></div>');
-        $(document).on('click', '.todoItem', function () {
-            $(this).remove();
-        });
-    });
-
     /*
     $.getJSON("https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1", function (result) {
         $('#greeting').html(result.data.children[0].data.title + ' <a href="' + result.data.children[0].data.url + '">&#119101;</a>');
