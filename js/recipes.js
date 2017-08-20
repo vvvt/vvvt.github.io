9e6ea776217529a3465228c5000003eb0471fea1
vvvt <shiftvt@gmail.com>
2017-08-20 14:45:57 +0200

    ADD read first meal from JSON

--- js/recipes.js
+++ js/recipes.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var recJSON;
+
     $.ajaxSetup({
         beforeSend: function (xhr) {
             if (xhr.overrideMimeType) {
@@ -9,6 +11,23 @@ $(document).ready(function () {
     });
 
     $.getJSON("js/recipes.json", function (result) {
-        $('#recipe').append('<h2>' + result.recipes[0].name + '</h2>');
+        recJSON = result;
+    });
+
+
+    $('#rec1').click(function () {
+        $('#recipe').empty();
+        $('#recipe').append('<h2>' + recJSON.recipes[0].name + '</h2>');
+        $('#recipe').append('<div class="img-container"><img src="' + recJSON.recipes[0].imgLink + '" /></div>');
+        $('#recipe').append('<div id="ingredient-container"><div id="amount"></div><div id="ingredients"></div></div>');
+        for (i = 0; i < recJSON.recipes[0].ingredients.length; i++) {
+            $('#amount').append('<p>' + recJSON.recipes[0].ingredients[i].amount + '</p>');
+            $('#ingredients').append('<p>' + recJSON.recipes[0].ingredients[i].name + '</p>');
+        }
+        $('#recipe').append('<div id="instruction-container"></div>');
+        for (i = 0; i < recJSON.recipes[0].instructions.length; i++) {
+            $('#instruction-container').append('<h3>Schritt ' + recJSON.recipes[0].instructions[i].step + '</h3>');
+            $('#instruction-container').append('<p>' + recJSON.recipes[0].instructions[i].text + '</p>');
+        }
     });
 });
