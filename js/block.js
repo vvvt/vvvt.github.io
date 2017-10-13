7e226e4ee546726181494809c97e2256faf83141
vvvt <shiftvt@gmail.com>
2017-10-14 00:30:32 +0200

    ADD search bar

--- js/block.js
+++ js/block.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function () {
     'use strict';
-
+    
     $.ajaxSetup({
         beforeSend: function (xhr) {
             if (xhr.overrideMimeType) {
@@ -17,7 +17,9 @@ $(document).ready(function () {
 
     $.getJSON("js/bookmarks.json", function (result) {
         for (i = 0; i < result.bookmarks.length; i++) {
-            $('#navbar').append("<a class=\"nav-item\" href=\"" + result.bookmarks[i].link + "\">" + "<p class=\"linktext\">" + result.bookmarks[i].title + "</p></a>");
+            $('#navbar').append('<a class="nav-item" href="' + result.bookmarks[i].link + '"><p class="linktext">' + result.bookmarks[i].title + '</p></a>');
+            /*
+            $('<a href="' + result.bookmarks[i].link + '></a>').addClass('nav-item').append('<p class="linktext">' + result.bookmarks[i].title + '</p>').appendTo('#navbar');*/
         }
     });
 
@@ -39,17 +41,4 @@ $(document).ready(function () {
     $.getJSON("https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1", function (result) {
         $('#tilText').html(result.data.children[0].data.title + ' <a href="' + result.data.children[0].data.url + '">&#119101;</a>');
     });
-
-    /*
-    $.getJSON("https://www.reddit.com/r/OnePiece/hot.json?limit=1", function (result) {
-        var firstEntry = result.data.children[0].data.title.substring(19,20);
-        var secondEntry = result.data.children[1].data.title.substring(19,20);
-        
-        if(firstEntry !== "8" && secondEntry!== "8"){
-            $('#chapterStatus').html("New Chapter is offline")
-        }else{
-            $('#chapterStatus').html("New Chapter is online")
-        }
-    });
-    */
 });
