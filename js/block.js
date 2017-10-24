b197463614eb0b0d89d39c1155d9e9fce6d84144
vvvt <shiftvt@gmail.com>
2017-10-24 19:38:04 +0200

    FIX remove deprecated parts

--- js/block.js
+++ js/block.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function () {
     'use strict';
-    
+
     $.ajaxSetup({
         beforeSend: function (xhr) {
             if (xhr.overrideMimeType) {
@@ -18,24 +18,20 @@ $(document).ready(function () {
     $.getJSON("js/bookmarks.json", function (result) {
         for (i = 0; i < result.bookmarks.length; i++) {
             $('#navbar').append('<a class="nav-item" href="' + result.bookmarks[i].link + '"><p class="linktext">' + result.bookmarks[i].title + '</p></a>');
-            /*
-            $('<a href="' + result.bookmarks[i].link + '></a>').addClass('nav-item').append('<p class="linktext">' + result.bookmarks[i].title + '</p>').appendTo('#navbar');*/
         }
     });
 
+
     $('.nav-item').mouseenter(function () {
-        if (window.matchMedia("(min-width: 501px)").matches) {
-            $(this).css({
-                "background": "linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0))"
-            });
-        }
+        alert("mouseenter");    
+        $(this).css({
+            "background": "linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0))"
+        });
     });
     $('.nav-item').mouseleave(function () {
-        if (window.matchMedia("(min-width: 501px)").matches) {
-            $(this).css({
-                "background": "linear-gradient(to bottom, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0))"
-            });
-        }
+        $(this).css({
+            "background": "linear-gradient(to bottom, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0))"
+        });
     });
 
     $.getJSON("https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1", function (result) {
