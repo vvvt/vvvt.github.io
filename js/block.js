d816abe8d48728a6c29d93ecad913a0f9d9d7f1f
vvvt <shiftvt@gmail.com>
2017-11-06 14:19:41 +0100

    EDIT rearrange TIL Box

--- js/block.js
+++ js/block.js
@@ -3,13 +3,12 @@
 
 $(document).ready(function () {
     'use strict';
-    
+
     $.ajaxSetup({
         beforeSend: function (xhr) {
             if (xhr.overrideMimeType) {
                 xhr.overrideMimeType("application/json");
             }
-            console.log("ajaxSetup done");
         }
     });
 
@@ -18,27 +17,10 @@ $(document).ready(function () {
     $.getJSON("js/bookmarks.json", function (result) {
         for (i = 0; i < result.bookmarks.length; i++) {
             $('#navbar').append('<a class="nav-item" href="' + result.bookmarks[i].link + '"><p class="linktext">' + result.bookmarks[i].title + '</p></a>');
-            /*
-            $('<a href="' + result.bookmarks[i].link + '></a>').addClass('nav-item').append('<p class="linktext">' + result.bookmarks[i].title + '</p>').appendTo('#navbar');*/
-        }
-    });
-
-    $('.nav-item').mouseenter(function () {
-        if (window.matchMedia("(min-width: 501px)").matches) {
-            $(this).css({
-                "background": "linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0))"
-            });
-        }
-    });
-    $('.nav-item').mouseleave(function () {
-        if (window.matchMedia("(min-width: 501px)").matches) {
-            $(this).css({
-                "background": "linear-gradient(to bottom, rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0))"
-            });
         }
     });
 
     $.getJSON("https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1", function (result) {
-        $('#tilText').html(result.data.children[0].data.title + ' <a href="' + result.data.children[0].data.url + '">&#119101;</a>');
+        $('#navbar').append('<a id="til-wrapper" class="nav-item" href="' + result.data.children[0].data.url + '"><p id="til-text" class="linktext">' + result.data.children[0].data.title + '</p></a>');
     });
 });
