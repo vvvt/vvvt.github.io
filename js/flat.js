ce71655fc07c7ba44fb7e064322d431bb3632bb5
vvvt <shiftvt@gmail.com>
2017-11-26 18:19:46 +0100

    FIX formatting

--- js/flat.js
+++ js/flat.js
@@ -1,26 +1,32 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 /*global $, document*/
 
-$(document).ready(function() {
-  'use strict';
+$(document).ready(function () {
+    'use strict';
 
-  $.ajaxSetup({
-    beforeSend: function(xhr) {
-      if (xhr.overrideMimeType) {
-        xhr.overrideMimeType("application/json");
-      }
-    }
-  });
+    $.ajaxSetup({
+        beforeSend: function (xhr) {
+            if (xhr.overrideMimeType) {
+                xhr.overrideMimeType("application/json");
+            }
+        }
+    });
 
-  var i;
 
-  $.getJSON("js/flat.json", function(result) {
-    for (i = 0; i < result.bookmarks.length; i++) {
-      $('#bookmarks-wrapper').append('<a class="bookmark circular" href="' + result.bookmarks[i].link + '"><img src="' + result.bookmarks[i].img + '" /></a>');
-    }
-  });
 
-  $.getJSON("https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1", function(result) {
-    $('#til').append('<a href="' + result.data.children[0].data.url + '"><p class="til-text">' + result.data.children[0].data.title + '</p></a>');
-  });
+    var i;
+
+    $.getJSON("js/flat.json", function (result) {
+        for (i = 0; i < result.bookmarks.length; i++) {
+            $('#bookmarks-wrapper').append('<a class="bookmark circular" href="' + result.bookmarks[i].link + '"><img class="icon" src="' + result.bookmarks[i].img + '" /></a>');
+        }
+    });
+
+    $.getJSON("https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1", function (result) {
+        $('#til').append('<a href="' + result.data.children[0].data.url + '"><p class="til-text">' + result.data.children[0].data.title + '</p></a>');
+    });
+
+    $('#search-button').click(function () {
+        console.log($('#searchbar').val());
+    });
 });
