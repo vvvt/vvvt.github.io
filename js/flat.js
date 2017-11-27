25533e6fee2b95bf4bbbaef5339f846711f15d31
vvvt <shiftvt@gmail.com>
2017-11-27 01:42:48 +0100

    ADD search prototype

--- js/flat.js
+++ js/flat.js
@@ -7,26 +7,48 @@ $(document).ready(function () {
     $.ajaxSetup({
         beforeSend: function (xhr) {
             if (xhr.overrideMimeType) {
-                xhr.overrideMimeType("application/json");
+                xhr.overrideMimeType('application/json');
             }
         }
     });
 
 
 
-    var i;
+    var i, search;
 
-    $.getJSON("js/flat.json", function (result) {
+    $.getJSON('js/flat.json', function (result) {
         for (i = 0; i < result.bookmarks.length; i++) {
-            $('#bookmarks-wrapper').append('<a class="bookmark circular" href="' + result.bookmarks[i].link + '"><img class="icon" src="' + result.bookmarks[i].img + '" /></a>');
+            $('#bookmarks').append('<a class="bookmarks bookmark-item circular" href="' + result.bookmarks[i].link + '"><img class="icon" src="' + result.bookmarks[i].img + '" /></a>');
         }
     });
 
-    $.getJSON("https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1", function (result) {
+    $.getJSON('https://www.reddit.com/r/todayilearned/top.json?t=hour&limit=1', function (result) {
         $('#til').append('<a href="' + result.data.children[0].data.url + '"><p class="til-text">' + result.data.children[0].data.title + '</p></a>');
     });
 
+    $.getJSON('js/search.json', function (result) {
+        search = result;
+        console.log(search.engines.length);
+    });
+
+    var firstSubstring = function (subject) {
+        for (i = 0; subject.length; i++) {
+            if (subject.substr(i, 1) === " ") {
+                return subject.substr(0, i);
+            }
+        }
+        return subject;
+    };
+
+    $('#searchbar').keyup(function (event) {
+        if (event.keyCode === 13) {
+            $('#search-button').click();
+        } else if (event.keyCode === 32) {
+            console.log(firstSubstring($('#searchbar').val()));
+        }
+    });
+
     $('#search-button').click(function () {
-        console.log($('#searchbar').val());
+        window.location.assign('http://www.mozilla.org');
     });
 });
