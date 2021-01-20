62e5e83695ca3da124714770c8bd9faacd30239b
Vincent Thiele <vincent_julius.thiele@mailbox.tu-dresden.de>
2021-01-20 13:27:14 +0100

    ADD new version

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -4,9 +4,9 @@
 // 1 - paint; 2 - fill; 3 - shape
 let activeMode = 1;
 let activeColor;
-let activeBrushSize;
+let canvas;
 
-init();
+buildInterface();
 
 const c = document.getElementById('canvas');
 const ctx = c.getContext('2d');
@@ -23,97 +23,165 @@ const buttonShapeCancel = document.querySelector("#btnShapeCancel");
 const shapeModal = document.querySelector("#shapeModal");
 const canvasContainer = document.querySelector("#canvasContainer");
 
-let currentPosition = { x: 0, y: 0 };
-
-canvasContainer.addEventListener('touchmove', draw);
-canvasContainer.addEventListener('touchstart', updatePosition);
-
-buttonSave.addEventListener("click", () => alert("Save Artwork"));
+buttonSave.addEventListener("click", () => window.location.href = './index.html');
 buttonCancel.addEventListener("click", () => alert("Cancel Editin;g"));
 buttonUndo.addEventListener("click", () => alert("Undo last Step"));
 buttonModePaint.addEventListener("click", () => changeMode(1, buttonModePaint));
 buttonModeFill.addEventListener("click", () => changeMode(2, buttonModeFill));
-buttonModeShape.addEventListener("click", () => toggleShapeModal());
-buttonShapeAccept.addEventListener("click", () => toggleShapeModal());
-buttonShapeCancel.addEventListener("click", () => toggleShapeModal());
+buttonModeShape.addEventListener("click", () => {
+	changeMode(3, buttonModeShape);
+	toggleShapeModal();
+});
+buttonShapeAccept.addEventListener("click", appendShape);
+buttonShapeCancel.addEventListener("click", toggleShapeModal);
 
+/**
+ * Set up interface components by adding buttons and a canvas.
+ */
+function buildInterface() {
+	const buttonWrapperColor = document.querySelector('#buttonWrapperColor');
+	const buttonWrapperMode = document.querySelector('#buttonWrapperMode');
+	const shapeGallery = document.querySelector('#shapeGallery');
+	const canvasContainer = document.querySelector("#canvasContainer");
+
+	// Place canvas according to container size
+	const newCanvas = document.createElement('canvas');
+	newCanvas.id = 'canvas';
+	newCanvas.width = canvasContainer.clientWidth;
+	newCanvas.height = canvasContainer.clientHeight;
+	canvasContainer.append(newCanvas);
+	canvas = new fabric.Canvas('canvas');
+	canvas.allowTouchScrolling = false;
+	canvas.fireRightClick = false;
+	canvas.selection = false;
+	canvas.isDrawingMode = true;
+
+	// Place brush size buttons
+	fetch('./assets/js/brushes.json')
+		.then(response => response.json())
+		.then(brushes => brushes.forEach(brush => {
+			const button = document.createElement('button');
+			button.id = `btnBrush${brush.name.toUpperCase()}`;
+			button.classList.add('btn-size');
+			button.style.backgroundSize = `${brush.size}px`;
+			if (brush.active) {
+				button.classList.add('active');
+				canvas.freeDrawingBrush.width = brush.size;
+			}
+			button.onclick = () => {
+				changeSize(brush.size, button);
+			};
+			buttonWrapperMode.append(button);
+		}));
+
+	// Place brush color buttons
+	fetch('./assets/js/colors.json')
+		.then(response => response.json())
+		.then(colors => colors.forEach(color => {
+			const button = document.createElement('button');
+			button.id = `btnColor${color.name}`;
+			button.classList.add('btn-color');
+
+			// The white button should have a white glow 
+			color.code === '#ffffff'
+				? (button.style.color = '#000000')
+				: (button.style.color = color.code);
+			button.style.backgroundColor = color.code;
+			if (color.active) {
+				button.classList.add('active');
+				canvas.freeDrawingBrush.color = color.code;
+			}
+			button.onclick = () => {
+				changeColor(color.code, button);
+			};
+
+			buttonWrapperColor.append(button);
+		}));
+
+	// Populate shape menu
+	fetch('./assets/js/shapes.json')
+		.then(response => response.json())
+		.then(shapes => shapes.forEach(shape => {
+			const div = document.createElement('div');
+			const img = document.createElement('img');
+			img.id = `shape_${shape.name}`
+			img.src = shape.img;
+			div.append(img);
+			div.classList.add('shape-gallery-entry')
+			div.addEventListener('click', () => {
+				const activeGalleryElements = document.querySelector('.shape-gallery-entry.active');
+				activeGalleryElements && activeGalleryElements.classList.remove('active');
+				div.classList.add('active');
+			});
+			shapeGallery.append(div);
+		}));
+}
+
+/**
+ * Toggle visibility of the shape menu
+ */
 function toggleShapeModal() {
 	shapeModal.classList.toggle("hidden");
 	canvasContainer.classList.toggle("hidden");
 }
 
 /**
- * Set active mode (1) drawing, (2) filling, (3) shapes
+ * Set active mode (1) drawing, (2) filling
  *
- * @param newMode selected mode
+ * @param mode selected mode
  * @param activeButton pressed button, to be set active
  */
-function changeMode(newMode, activeButton) {
+function changeMode(mode, activeButton) {
 	const modeButtons = document.querySelectorAll('.btn-mode');
 	modeButtons.forEach(button => button.classList.remove('active'));
 
-	activeMode = newMode;
-	activeButton.classList.add('active');
-
-	if (activeMode === 3) {
-		alert('Formenfenster öffnen');
+	switch (mode) {
+		case 1:
+			canvas.isDrawingMode = true;
+			console.log("switched to mode 1");
+			break;
+		case 2:
+			canvas.isDrawingMode = false;
+			canvas.getObjects().forEach(object => object.selectable = false)
+			console.log("switched to mode 2");
+			break;
+		case 3:
+			canvas.isDrawingMode = false;
+			canvas.getObjects().forEach(object => object.selectable = false)
+			console.log("switched to mode 3");
+			break;
 	}
+
+	activeMode = mode;
+	activeButton.classList.add('active');
 }
 
 /**
  * Set active color
  *
- * @param newColor selected color
+ * @param color selected color
  * @param activeButton pressed button, to be set active
  */
-function changeColor(newColor, activeButton) {
+function changeColor(color, activeButton) {
 	const colorButtons = document.querySelectorAll('.btn-color');
 	colorButtons.forEach(button => button.classList.remove('active'));
 
-	activeColor = newColor;
+	canvas.freeDrawingBrush.color = color;
 	activeButton.classList.add('active');
 }
 
 /**
  * Set active brush size
  *
- * @param newBrushSize selected brush size
+ * @param brushSize selected brush size
  * @param activeButton pressed button, to be set active
  */
-function changeSize(newBrushSize, activeButton) {
+function changeSize(brushSize, activeButton) {
 	const sizeButtons = document.querySelectorAll('.btn-size');
 	sizeButtons.forEach(button => button.classList.remove('active'));
 
-	activeBrushSize = newBrushSize;
+	canvas.freeDrawingBrush.width = brushSize;
 	activeButton.classList.add('active');
-}
-
-/**
- * Update mouse position
- *
- * @param {object} e event
- */
-function updatePosition(e) {
-	currentPosition.x = e.touches[0].pageX - canvas.offsetLeft;
-	currentPosition.y = e.touches[0].pageY - canvas.offsetTop;
-}
-
-/**
- * Draw line in canvas
- *
- * @param {object} e event
- */
-function draw(e) {
-	ctx.beginPath();
-	ctx.lineWidth = activeBrushSize;
-	ctx.lineCap = 'round';
-	ctx.strokeStyle = activeColor;
-
-	ctx.moveTo(currentPosition.x, currentPosition.y);
-	updatePosition(e);
-	ctx.lineTo(currentPosition.x, currentPosition.y);
-
-	ctx.stroke();
 }
 
 /**
@@ -123,4 +191,26 @@ function draw(e) {
 function fill() {
 	const canvas = document.getElementById('canvas');
 	canvas.style.backgroundColor = activeColor;
+}
+
+/**
+ * 
+ */
+function appendShape() {
+	const selectedShape = document.querySelector(".shape-gallery-entry.active img");
+	selectedShape && console.log(selectedShape);
+
+	fabric.loadSVGFromURL(selectedShape.src, shapes => {
+		let shape = new fabric.Group(shapes.filter(s => s.fill));
+		console.log(shapes.filter(s => s.fill));
+		shape.id = `object_${selectedShape.id}`;
+		shape.selectable = false;
+		shape.scaleY = 20;
+		shape.scaleX = 20;
+		//oImg.hasControls = false;
+		canvas.setActiveObject(shape);
+		canvas.add(shape);
+	})
+
+	toggleShapeModal()
 }
